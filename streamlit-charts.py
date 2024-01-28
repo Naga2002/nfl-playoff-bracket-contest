ac67131fa858f491d042749ece1118c63a93d9e9
Nathan Mott <nmott@phdata.io>
2024-01-28 14:21:08 -0500

    Add team colors; fix San Fran spelling

--- streamlit-charts.py
+++ streamlit-charts.py
@@ -49,11 +49,47 @@ def main():
 
     superbowl_appears = build_superbowl_appears_df(entries_data)
 
+    nfl_team_colors = nfl_team_colors = {
+            'Arizona Cardinals': '#8B0021',
+            'Atlanta Falcons': '#A71930',
+            'Baltimore Ravens': '#241773',
+            'Buffalo Bills': '#00338D',
+            'Carolina Panthers': '#0085CA',
+            'Chicago Bears': '#0B162A',
+            'Cincinnati Bengals': '#FB4F14',
+            'Cleveland Browns': '#311D00',
+            'Dallas Cowboys': '#FFFFFF',
+            'Denver Broncos': '#002244',
+            'Detroit Lions': '#0076B6',
+            'Green Bay Packers': '#203731',
+            'Houston Texans': '#03202F',
+            'Indianapolis Colts': '#002C5F',
+            'Jacksonville Jaguars': '#006778',
+            'Kansas City Chiefs': '#E31837',
+            'Las Vegas Raiders': '#000000',
+            'Los Angeles Chargers': '#002A5E',
+            'Los Angeles Rams': '#002244',
+            'Miami Dolphins': '#008E97',
+            'Minnesota Vikings': '#4F2683',
+            'New England Patriots': '#002244',
+            'New Orleans Saints': '#D3BC8D',
+            'New York Giants': '#0B2265',
+            'New York Jets': '#125740',
+            'Philadelphia Eagles': '#004C54',
+            'Pittsburgh Steelers': '#FFB612',
+            'San Francisco 49ers': '#B3995D',
+            'Seattle Seahawks': '#002244',
+            'Tampa Bay Buccaneers': '#D50A0A',
+            'Tennessee Titans': '#4B92DB',
+            'Washington Commanders': '#773141'
+            }
+
+
     superbowl_picks_fig = px.bar(superbowl_picks, x='count', y='superbowl-winner', text='percent', color='superbowl-winner'
-                 ,title='Super Bowl Winners', orientation='h')
+                 ,title='Super Bowl Winners', orientation='h', color_discrete_map=nfl_team_colors)
     
     superbowl_appears_fig = px.bar(superbowl_appears, x='count', y='superbowl-teams', text='percent', color='superbowl-teams'
-                 ,title='Super Bowl Team Appearances', orientation='h')    
+                 ,title='Super Bowl Team Appearances', orientation='h', color_discrete_map=nfl_team_colors)    
 
     superbowl_picks_fig.update_layout(showlegend=False)
     superbowl_appears_fig.update_layout(showlegend=False)
