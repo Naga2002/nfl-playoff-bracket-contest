99496b3b392c4b64a72e1cc709f2b997e512d86d
Nathan Mott <nmott@phdata.io>
2024-01-28 14:49:36 -0500

    Added conference charts

--- streamlit-charts.py
+++ streamlit-charts.py
@@ -27,12 +27,30 @@ def build_superbowl_picks_df(entries_data):
     return group_counts_percent(entries_data, 'superbowl-winner')
 
 def build_superbowl_appears_df(entries_data):
-    # Concat series of both column values
+    # Concat series of column values
     series1 = entries_data['superbowl-team_1']
     series2 = entries_data['superbowl-team_2']
     unioned_df = pd.concat([series1, series2]).to_frame(name = 'superbowl-teams')
 
     return group_counts_percent(unioned_df, 'superbowl-teams')
+
+def build_conference_picks_df(entries_data):
+    # Concat series of column values
+    series1 = entries_data['conference1-winner']
+    series2 = entries_data['conference2-winner']
+    unioned_df = pd.concat([series1, series2]).to_frame(name = 'conference-winners')     
+
+    return group_counts_percent(unioned_df, 'conference-winners')   
+
+def build_conference_appears_df(entries_data):
+    # Concat series of column values
+    series1 = entries_data['conference1-team_1']
+    series2 = entries_data['conference1-team_2']
+    series3 = entries_data['conference2-team_1']
+    series4 = entries_data['conference2-team_2']        
+    unioned_df = pd.concat([series1, series2, series3, series4]).to_frame(name = 'conference-teams')     
+
+    return group_counts_percent(unioned_df, 'conference-teams')   
 
 def build_figure(input_df, title, y_name, nfl_team_colors):
     fig = px.bar(input_df, x='count', y=y_name, text='percent', color=y_name
@@ -51,10 +69,6 @@ def main():
 
     # Remove the "actual" entry if already added
     entries_data.drop('actual', inplace = True)
-
-    superbowl_picks = build_superbowl_picks_df(entries_data)
-
-    superbowl_appears = build_superbowl_appears_df(entries_data)
 
     nfl_team_colors  = {
             'Arizona Cardinals': '#8B0021',
@@ -91,19 +105,31 @@ def main():
             'Washington Commanders': '#773141'
             }
 
+    # build each graph dataframe
+    superbowl_picks = build_superbowl_picks_df(entries_data)
+    superbowl_appears = build_superbowl_appears_df(entries_data)
+    conference_picks = build_conference_picks_df(entries_data)
+    conference_appears = build_conference_appears_df(entries_data)
 
+    # build each plottly figure
     superbowl_picks_fig = build_figure(superbowl_picks, 'Super Bowl Winners', 'superbowl-winner', nfl_team_colors)
-    
     superbowl_appears_fig = build_figure(superbowl_appears, 'Super Bowl Team Appearances', 'superbowl-teams', nfl_team_colors)
-       
+    conference_picks_fig = build_figure(conference_picks, 'Conference Winners', 'conference-winners', nfl_team_colors)
+    conference_appears_fig = build_figure(conference_appears, 'Conference Team Appearances', 'conference-teams', nfl_team_colors)
+
+    # set tabs for streamlit   
     tab_sb, tab_conf, tab_div, tab_wc = st.tabs(['Superbowl', 'Conference', 'Divisional', 'Wild Card'])
 
+    # build graphs in columns on each tab
     with tab_sb:
         col1, col2 = st.columns(2)
         col1.plotly_chart(superbowl_picks_fig, use_container_width=True, )
         col2.plotly_chart(superbowl_appears_fig, use_container_width=True, showlegend=False)
 
-    tab_conf.write('comming soon')
+    with tab_conf:
+        col1, col2 = st.columns(2)
+        col1.plotly_chart(conference_picks_fig, use_container_width=True, )
+        col2.plotly_chart(conference_appears_fig, use_container_width=True, showlegend=False)
 
 
 if __name__ == '__main__':
